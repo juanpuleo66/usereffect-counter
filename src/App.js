21e3a362cc2f5a89b5ce1b8d86b96a96202189f6
juanpuleo66 <juanpuleo@gmail.com>
2019-10-29 23:25:07 -0400

    102919-1125-PM

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, Fragment  } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect( () => {
+    console.count('In usserEffect, after render')
+    console.log(`Count: ${count}`)
+  })
+
+  console.count('Rendering')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <button onClick={ () => setCount( (c) => c-1)}>-</button>
+      <h1>Count: {count}</h1>
+      <button onClick={ () => setCount( (c) => c+1)}>+</button>
+    </Fragment>
   );
 }
 
